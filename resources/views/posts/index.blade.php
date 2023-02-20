0fc0a570384c8acad4cfb340f17cfde471270068
Salbih Mustafa <salbih.mustafa@certegy.com>
2023-02-20 10:37:24 -0600

    initial commit

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('title', 'Blog Posts')
+
+@section('content')
+    @forelse($posts as $key => $post)
+        @include('posts.partials.post')
+    @empty
+        No posts found!
+    @endforelse
+@endsection
