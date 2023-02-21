9eab40e1bc8cca94ed451f8763e8adcfc747001b
Salbih Mustafa <salbih.mustafa@certegy.com>
2023-02-21 12:16:03 -0600

    create posts controller using resource

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AboutController;
+use App\Http\Controllers\PostsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,67 +17,46 @@ use App\Http\Controllers\AboutController;
 */
 
 Route::get('/', [HomeController::class, 'home'])->name('home.index');
-
 Route::get('/contact', [HomeController::class, 'contact'])->name('home.contact');
 
 Route::get('/single', AboutController::class);
 
-$posts = [
-    1 => [
-        'title' => 'Intro to Laravel',
-        'content' => 'This is a short intro to Laravel',
-        'is_new' => true,
-        'has_comments' => true
-    ],
-    2 => [
-        'title' => 'Intro to PHP',
-        'content' => 'This is a short intro to PHP',
-        'is_new' => false
-    ]
-];
-
-Route::get('/posts', function () use ($posts) {
-    return view('posts.index', ['posts' => $posts]);
-});
-
-Route::get('/posts/{id}', function ($postId) use ($posts) {
-    abort_if(!isset($posts[$postId]), 404);
-    return view('posts.show', ['post' => $posts[$postId]]);
-})->name('posts.show');
+//first parameter is route name
+Route::resource('posts', PostsController::class)->only(['index', 'show']);
 
 Route::get('/recent-posts/{days_ago?}', function ($daysAgo = 20) {
     return "Posts from " . $daysAgo . " days ago";
 })->name('posts.recent.index')->middleware('auth');
 
-Route::prefix('/fun')->name('fun.')->group(function () use ($posts) {
-    Route::get('/responses', function () use ($posts) {
-        return response($posts, 201)->header('Content-Type', 'application/json')->cookie('MY_COOKIE', 'Salbih Mustafa', 3600);
-    })->name('responses');
-
-    Route::get('/redirect', function () {
-        return redirect('/contact');
-    })->name('redirect');
-
-    Route::get('/back', function () {
-        return back();
-    })->name('back');
-
-    Route::get('/named-route', function () {
-        return redirect()->route('posts.show', ['id' => 1]);
-    })->name('named-route');
-
-    Route::get('/away', function () {
-        return redirect()->away('https://google.com');
-    })->name('away');
-
-    Route::get('/json', function () use ($posts) {
-        return response()->json($posts);
-    })->name('json');
-
-    Route::get('/download', function () {
-        return response()->download(public_path('/daniel.jpg'), 'face.jpg');
-    })->name('download');
-});
+//Route::prefix('/fun')->name('fun.')->group(function () use ($posts) {
+//    Route::get('/responses', function () use ($posts) {
+//        return response($posts, 201)->header('Content-Type', 'application/json')->cookie('MY_COOKIE', 'Salbih Mustafa', 3600);
+//    })->name('responses');
+//
+//    Route::get('/redirect', function () {
+//        return redirect('/contact');
+//    })->name('redirect');
+//
+//    Route::get('/back', function () {
+//        return back();
+//    })->name('back');
+//
+//    Route::get('/named-route', function () {
+//        return redirect()->route('posts.show', ['id' => 1]);
+//    })->name('named-route');
+//
+//    Route::get('/away', function () {
+//        return redirect()->away('https://google.com');
+//    })->name('away');
+//
+//    Route::get('/json', function () use ($posts) {
+//        return response()->json($posts);
+//    })->name('json');
+//
+//    Route::get('/download', function () {
+//        return response()->download(public_path('/daniel.jpg'), 'face.jpg');
+//    })->name('download');
+//});
 
 
 
