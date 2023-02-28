79a3dea3f18c84a6260f9672cc5268442ea36220
Salbih Mustafa <salbih.mustafa@certegy.com>
2023-02-28 15:21:24 -0600

    created edit blade and added method PUT

---
+++ resources/views/posts/edit.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('title', 'Edit the Post')
+
+@section('content')
+    <form action="{{ route('posts.update', ['post' => $post->id]) }}" method="POST">
+        @csrf
+        @method('PUT')
+        @include('posts.partials.form')
+        <div>
+            <input type="submit" value="Update">
+        </div>
+    </form>
+@endsection
--- resources/views/posts/partials/form.blade.php
+++ resources/views/posts/partials/form.blade.php
@@ -1,13 +1,13 @@
 <div>
     <label for="title">Title:</label>
-    <input type="text" name="title" id="title" value=" {{ old('title') }}">
+    <input type="text" name="title" id="title" value=" {{ old('title', optional($post ?? null)->title) }}">
 </div>
 @error('title')
 <div>{{ $message }}</div>
 @enderror
 <div>
     <label for="content">Content:</label>
-    <textarea name="content" id="content">{{ old('content') }}</textarea>
+    <textarea name="content" id="content">{{ old('content', optional($post ?? null)->content) }}</textarea>
 </div>
 @error('content')
 <div>{{ $message }}</div>
