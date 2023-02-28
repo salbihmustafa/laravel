f77828d94f30bea1687f65fa82d15f2a3c3a3312
Salbih Mustafa <salbih.mustafa@certegy.com>
2023-02-28 15:58:24 -0600

    create delete form

--- resources/views/posts/partials/post.blade.php
+++ resources/views/posts/partials/post.blade.php
@@ -1,1 +1,8 @@
-<div>{{ $key }}. Title: {{ $post['title'] }} ID: {{ $post['id'] }}</div>
+<div>{{ $post->id }}. Title: {{ $post->title }}</div>
+<div>
+    <form action="{{ route('posts.destroy', ['post' => $post->id]) }}" method="POST">
+        @csrf
+        @method('DELETE')
+        <input type="submit" value="Delete">
+    </form>
+</div>
